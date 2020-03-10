9db4921a3bb4564cd1048d6f962c9d6b5fd5275a
SarmadAliQureshi <sarmadalikaharian@gmail.com>
2020-03-10 18:21:11 +0500

    tuesday1

--- routes/allinvoices.js
+++ routes/allinvoices.js
@@ -25,10 +25,11 @@ let db = new sqlite3.Database('./database/main.db', function(err){
 
 
 //To get all the data from database
-router.get('/invoices',function(req,res){
-    if ("customer_name" in req.query){
-        console.log("Name Found",req.query.name)
-        db.all(`select * from invoices where customer_name="${req.query.customer_name}" COLLATE NOCASE`,function(err,rows){
+router.get('/allinvoices',function(req,res){
+
+    if ("customer_id" in req.query && "product_id" in req.query){
+        console.log("Name Found In",req.query.customer_id)
+        db.all(`select * from allinvoices where customer_id="${req.query.customer_id}" and product_id="${req.query.product_id}" ORDER by date_time DESC limit 1`,function(err,rows){
             if (err){
                 console.log('a')
                 res.send(err.message)
@@ -44,9 +45,10 @@ router.get('/invoices',function(req,res){
             }
         })
     }
+
     else if ("invoice_id" in req.query){
-        console.log("Name Found",req.query.id)
-        db.all(`select * from invoices where invoice_id="${req.query.invoice_id}" COLLATE NOCASE`,function(err,rows){
+        console.log("Name Found",req.query)
+        db.all(`select * from allinvoices where invoice_id="${req.query.invoice_id}" ORDER by date_time DESC`,function(err,rows){
             if (err){
                 console.log('a')
                 res.send(err.message)
@@ -62,22 +64,48 @@ router.get('/invoices',function(req,res){
             }
         })
     }
+    else if ("customer_id" in req.query){
+        console.log("Name Found :",req.query.customer_id)
+        console.log(req.query['customer_id'])
+        var sql =`select * from allinvoices where customer_id=${req.query.customer_id} ORDER by date_time DESC`
+        console.log(sql)
+        db.all(sql,function(err,rows){
+            console.log('l',rows)
+            if (err){
+                console.log('a')
+                res.send(err.message)
+                return console.error(err.message)
+            }
+            else if (rows.length==0){
+            console.log('b')
+            console.log(rows.length)
+            res.send({result:"No data found!"})
+            }
+            else{
+                res.send(rows)
+            }
+        })
+    }
+    // selecting the last price at which customer purchased the item
     else{
+    console.log("IN ELSE")    
     console.log("req param",req.query)
-    db.all('select * from invoices order by customer_name asc',function(err,row){
-        // console.log(row)
-        var output = [];
-        row = row.map(function(item){
-            // console.log();
-            output.push(Object.values(item))
-        }, row)
-        res.send(output)
+    db.all('select * from allinvoices',function(err,row){
+        if (err){
+            console.log('a')
+            res.send(err.message)
+            return console.error(err.message)
+        }
+        else{
+            console.log('data',row)
+            res.send(row)
+        }
     
     })
 }
 })
 
-router.post('/invoices',function(req,res){
+router.post('/allinvoices',function(req,res){
     console.log("IN post req")
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -109,7 +137,7 @@ router.post('/invoices',function(req,res){
     console.log('today',today)
     console.log('date',date)
     // sql = `insert into invoices(${column_names}) values(${req.body.product_id},'${req.body.product_name}',${req.body.product_price})`
-    sql = `insert into invoices(${column_names}) values(${newdata})`
+    sql = `insert into allinvoices(${column_names}) values(${newdata})`
     console.log(sql)
     console.log("a",sql,column_names,newdata)
     db.run(sql, function(err) {
@@ -126,7 +154,7 @@ router.post('/invoices',function(req,res){
     // res.end()
 })
 
-router.put('/invoices',function(req,res){
+router.put('/allinvoices',function(req,res){
     console.log("In put request")
     console.log(req.body)
     console.log(req.query)
@@ -168,8 +196,38 @@ router.put('/invoices',function(req,res){
    
 
     console.log("All data : ",column_names,newdata)
-    if ("invoice_id" in req.query){
-        sql = `update  invoices set ${update_string}  where invoice_id=${req.query.invoice_id}`
+    if ("customer_id" in req.query && "invoice_id" in req.query){
+        sql = `update  allinvoices set ${update_string}  where customer_id=${req.query.customer_id} and invoice_id=${req.query.invoice_id}`
+        console.log(sql)
+        db.run(sql, function(err) {
+            if (err) {
+            res.send(JSON.stringify({error:err.message}))
+    
+              return console.error(err.message);
+            }
+            else{
+            res.send({success:`customer with id ${req.query.customer_id} updated`})
+    
+            console.log(`Rows updated`);}
+          })
+        }
+        else if ("customer_id" in req.query && "invoice_id" in req.query && "product_id" in req.query){
+            sql = `update  allinvoices set ${update_string}  where customer_id=${req.query.customer_id} and product_id=${req.query.product_id} and invoice_id=${req.query.invoice_id}`
+            console.log(sql)
+            db.run(sql, function(err) {
+                if (err) {
+                res.send(JSON.stringify({error:err.message}))
+        
+                  return console.error(err.message);
+                }
+                else{
+                res.send({success:`invoice with id ${req.query.invoice_id} updated`})
+        
+                console.log(`Rows updated`);}
+              })
+            }
+    else if ("invoice_id" in req.query){
+        sql = `update  allinvoices set ${update_string}  where invoice_id=${req.query.invoice_id}`
         console.log(sql)
         db.run(sql, function(err) {
             if (err) {
@@ -183,41 +241,24 @@ router.put('/invoices',function(req,res){
             console.log(`Rows updated`);}
           })
         }    
-    else if ("customer_id" in req.query){
-    sql = `update  invoices set ${update_string}  where customer_id=${req.query.customer_id}`
-    console.log(sql)
-    db.run(sql, function(err) {
-        if (err) {
-        res.send(JSON.stringify({error:err.message}))
+    // else if ("customer_id" in req.query){
+    // sql = `update  allinvoices set ${update_string}  where customer_id=${req.query.customer_id}`
+    // console.log(sql)
+    // db.run(sql, function(err) {
+    //     if (err) {
+    //     res.send(JSON.stringify({error:err.message}))
 
-          return console.error(err.message);
-        }
-        else{
-        res.send({success:`customer with id ${req.query.customer_id} updated`})
+    //       return console.error(err.message);
+    //     }
+    //     else{
+    //     res.send({success:`customer with id ${req.query.customer_id} updated`})
 
-        console.log(`Rows updated`);}
+    //     console.log(`Rows updated`);}
       })
-    }
-    else if ("customer_name" in req.query){
-        console.log(update_string)
-        sql = `update  invoices set ${update_string}  where customer_name='${req.query.customer_name}' COLLATE NOCASE`
-    console.log(sql)
-    db.run(sql, function(err) {
-        if (err) {
-            res.send(JSON.stringify({error:err.message}))
-            return console.error(err.message);
-        }
-        else{
-        res.send({success:`customer ${req.query.customer_name} updated`})
+    
+    
 
-        console.log(`Rows updated`);}
-      })
-    }
-    console.log(column_names+' : '+data)
-    console.log(req.body)
-})
-
-router.delete('/invoices',function(req,res){
+router.delete('/allinvoices',function(req,res){
     console.log("In delete request")
     console.log(req.body)
     console.log(req.query)
@@ -225,46 +266,30 @@ router.delete('/invoices',function(req,res){
     
    
     if ("invoice_id" in req.body){
-        sql = `delete from invoices where invoice_id=${req.body['invoice_id']}`
+        sql = `delete from allinvoices where invoice_id=${req.body['invoice_id']}`
         console.log(sql)
         db.run(sql, function(err) {
             if (err) {
               return console.error(err.message);
             }
             else{
-                res.send({success:`invoice with id ${req.body['customer_id']} deleted`})
+                res.send({success:`invoice with id ${req.body['invoice_id']} deleted`})
             }
             console.log(`Rows deleted`);
           })
         //   res.end()
         }    
     else if ("customer_id" in req.body){
-    sql = `delete from invoices where customer_id=${req.body['customer_id']}`
+    sql = `delete from allinvoices where customer_id=${req.body['customer_id']}`
     console.log(sql)
     db.run(sql, function(err) {
         if (err) {
           return console.error(err.message);
         }
         else{
-            res.send({success:`invoice with id ${req.body['customer_id']} deleted`})
+            res.send({success:`invoice with customer id ${req.body['customer_id']} deleted`})
         }
         console.log(`Rows deleted`);
-      })
-    //   res.end()
-    }
-    else if ("customer_name" in req.body){
-        // cname = req.body[]
-        sql = `delete from  invoices  where customer_name='${req.body['customer_name']}' COLLATE NOCASE`
-    console.log(sql)
-    db.run(sql, function(err) {
-        if (err) {
-          res.send(JSON.stringify({error:err.message}))
-
-          console.log(err.message);
-        }
-        else{
-        res.send({success:`invoices with customer name ${req.body['customer_name']} deleted`})
-        console.log(`Rows deleted`);}
       })
     //   res.end()
     }
